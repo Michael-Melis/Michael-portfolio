f99b5d71b85d96bb1a50335209b7e5ff10b9d391
MichaelMelis <michael.melis169@gmail.com>
2021-11-07 21:15:24 +0100

    feat: HeadResume done

--- src/components/HeadResume.js
+++ src/components/HeadResume.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
-import { faMobile } from "@fortawesome/free-solid-svg-icons";
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 import ButtonMailto from "./ButtonMailto";
 import styled from "styled-components";
 import { StyledButton } from "../styles";
@@ -14,7 +14,7 @@ const HeadResume = () => {
 
       <StyledContacts>
         <StyledIcons>
-          <FontAwesomeIcon size="2x" icon={faPaperPlane} />
+          <StyledIcon size="2x" icon={faPaperPlane} />
           <MailButton>
             <ButtonMailto
               label="michael.melis169@gmail.com"
@@ -23,42 +23,61 @@ const HeadResume = () => {
           </MailButton>
         </StyledIcons>
         <StyledIcons>
-          <FontAwesomeIcon size="2x" icon={faMobile} />
+          <StyledIcon size="2x" icon={faMobileAlt} />
           <h2>+421944045190</h2>
         </StyledIcons>
       </StyledContacts>
-      {/* <img src={pic} alt="Michael profile "/> */}
     </StyledHeadResume>
   );
 };
 const StyledHeadResume = styled.div`
-  padding: 0.8rem 3rem;
+  padding: 1.5rem 3rem;
   border-radius: 25px;
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: 8rem auto;
+  border: 5px solid #2dd1a0;
   background: #2dd1a0;
-  width: 80%;
+  width: 50%;
   color: #fff;
-  position: relative;
+
+  font-weight: lighter;
+
   h1 {
     text-align: center;
     font-size: 3rem;
     letter-spacing: 0.5rem;
+    margin-bottom: 1.5rem;
+  }
+  @media (max-width: 1300px) {
+    margin: 2rem 0rem;
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 100%;
   }
 `;
 const StyledContacts = styled.div`
   display: flex;
   justify-content: space-around;
+  width: 100%;
+  flex-wrap: wrap;
+  @media (max-width: 1300px) {
+  }
 `;
 const StyledIcons = styled.div`
   display: flex;
+  flex-direction: row;
+  margin-bottom: 1rem;
+  align-items: center;
 `;
-const MailButton = styled(StyledButton)`
-  margin: 0;
-  padding: 0;
-  padding-top: 0;
-  font-size: 1rem;
+const StyledIcon = styled(FontAwesomeIcon)`
+  margin-right: 1rem;
 `;
+const MailButton = styled(StyledButton)``;
 
 export default HeadResume;
--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 export const StyledButton = styled.div`
   text-decoration: none;
+
   color: #fff;
   display: flex;
   align-items: center;
-
   font-size: 2rem;
+  padding: 0rem 1rem;
   @media (max-width: 1300px) {
-    font-size: 1.5rem;
+    font-size: 1rem;
+    width: 100%;
+    padding: 0;
   }
 
   &:hover {
-    color: #2dd1a0;
+    background: #1d1d1d;
+    border-radius: 50px;
+    color: #fff;
   }
 `;

0eb8badbcf14914df8433efd7f26f4b2a8f81503
MichaelMelis <michael.melis169@gmail.com>
2021-11-09 19:54:25 +0100

    fix: removed unused component

--- src/components/ButtonMailto.js
+++ src/components/ButtonMailto.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { StyledButton, StyledResumeMailToButton } from "../styles";
+import { StyledButton } from "../styles";
 
 const ButtonMailto = ({ mailto, label }) => {
   return (

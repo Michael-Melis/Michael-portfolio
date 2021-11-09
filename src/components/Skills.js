1165852cc2be44ee8fbfbe9eedf1c8a43fe6da85
MichaelMelis <michael.melis169@gmail.com>
2021-11-09 17:37:13 +0100

    build: created Skills to add all skills in resume

---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skills = ({ skill }) => {
+  return (
+    <div>
+      <p>{skill}</p>
+      <p>circles</p>
+    </div>
+  );
+};
+export default Skills;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,60 @@
 import React from "react";
-import resume from "../img/cv.png";
-import styled from "styled-components";
 import HeadResume from "../components/HeadResume";
+import Skills from "../components/Skills";
 
 const Resume = () => {
   return (
     <div id="resume">
       <HeadResume />
-      <h2> skills-name</h2>
-      <p>Actual Skill</p>
+      <div>
+        <div>
+          <h2> EDUCATION</h2>
+          <p>2007 - 2011</p>
+          <p>GRAMMAR SCHOOL</p>
+          <p>in Liptovsky Mikulas, Slovakia</p>
+        </div>
+        <div>
+          <h2> LANGUEAGES</h2>
+          <Skills />
+          <Skills />
+          <Skills />
+        </div>
+        <div>
+          <h2>WORK EXPERIENCE</h2>
+          <div>
+            <p>
+              <span>AMAZON ASSOCIATE</span>
+            </p>
+            <p>AUG 2020 - AUG2021</p>
+            <p>AMAZON, MANCHESTER</p>
+          </div>
+          <div>
+            <p>
+              <span>BAR SUPERVISOR</span>
+            </p>
+            <p>DEC 2015 - AUG2020</p>
+            <p>BRITANNIA COUNTRY HOUSE HOTEL, MANCHESTER</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>FRONT-END SKILLS</h2>
+        <Skills skill="Figma" />
+        <Skills skill="HTML5" />
+        <Skills skill="CSS3" />
+        <Skills skill="JAVASCRIPT" />
+        <Skills skill="REACT JS" />
+        <Skills skill="GITHUB" />
+        <Skills skill="SASS" />
+        <Skills skill="SEO" />
+        <Skills skill="RESPONSIVE/MOBILE DESIGN" />
+        <Skills skill="GIT/VERSION CONTROL" />
+        <Skills skill="RECOIL JS" />
+        <Skills skill="ADOBE PHOTOSHOP" />
+        <Skills skill="ADOBE LIGHTROOM" />
+        <Skills skill="ADOBE PREMIERE PRO" />
+        <Skills skill="VIRTUAL CUT PRO" />
+      </div>
     </div>
   );
 };
